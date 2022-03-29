6c76eff017e4b43609231b58caf5d570b204f7ec
goops2000 <goops2000@naver.com>
2022-03-29 22:04:26 +0900

    minutes, hours 변환기 만들기

--- movie-app-practice/src/App.tsx
+++ movie-app-practice/src/App.tsx
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
+
+const Converter = () => {
+  const [minutes, setState] = useState(0);
+  const onChange = (event: any) => {
+    setState(event.target.value);
+  };
+
+  const [flipped, setFlipped] = useState(false);
+  const onFlip = () => {
+    setFlipped((current) => !current);
+    setState(0);
+  };
+  return (
+    <div>
+      <h1>Super Converter</h1>
+      <h3>Minutes</h3>
+      <input
+        value={flipped ? Math.round(minutes * 60) : minutes}
+        onChange={onChange}
+        placeholder="Minutes"
+        type="number"
+        disabled={flipped}
+      />
+      <h3>Hours</h3>
+      <input
+        type="number"
+        placeholder="Hours"
+        disabled={!flipped}
+        onChange={onChange}
+        value={flipped ? minutes : Math.round(minutes / 60)}
+      />
+      <p>
+        <button onClick={onFlip}>flip</button>
+      </p>
+    </div>
+  );
+};
 
 function App() {
   return (
     <div className="App">
-      <h1>Welcome back!</h1>
+      <Converter />
     </div>
   );
 }
