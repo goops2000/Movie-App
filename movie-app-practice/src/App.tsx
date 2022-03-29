2579eebbcf96539152cba730bd510dc766f4ff76
goops2000 <goops2000@naver.com>
2022-03-30 00:14:37 +0900

    converter 완성

--- movie-app-practice/src/App.tsx
+++ movie-app-practice/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-const Converter = () => {
+function MinutesToHours() {
   const [minutes, setState] = useState(0);
   const onChange = (event: any) => {
     setState(event.target.value);
@@ -11,36 +11,98 @@ const Converter = () => {
     setFlipped((current) => !current);
     setState(0);
   };
+
+  const onReset = () => {
+    setState(0);
+  };
   return (
-    <div>
-      <h1>Super Converter</h1>
-      <h3>Minutes</h3>
-      <input
-        value={flipped ? Math.round(minutes * 60) : minutes}
-        onChange={onChange}
-        placeholder="Minutes"
-        type="number"
-        disabled={flipped}
-      />
-      <h3>Hours</h3>
-      <input
-        type="number"
-        placeholder="Hours"
-        disabled={!flipped}
-        onChange={onChange}
-        value={flipped ? minutes : Math.round(minutes / 60)}
-      />
-      <p>
-        <button onClick={onFlip}>flip</button>
-      </p>
-    </div>
+    <>
+      <div>
+        <h3>Minutes</h3>
+        <input
+          value={flipped ? Math.round(minutes * 60) : minutes}
+          onChange={onChange}
+          placeholder="Minutes"
+          type="number"
+          disabled={flipped}
+        />
+      </div>
+      <div>
+        <h3>Hours</h3>
+        <input
+          type="number"
+          placeholder="Hours"
+          disabled={!flipped}
+          onChange={onChange}
+          value={flipped ? minutes : Math.round(minutes / 60)}
+        />
+      </div>
+      <span>
+        <button onClick={onFlip}>{flipped ? "Turn back" : "Invert"}</button>
+      </span>
+      <span>
+        <button onClick={onReset}>reset</button>
+      </span>
+    </>
   );
-};
+}
+
+function KmToM() {
+  const [distance, setDistance] = useState(0);
+  const onDistance = (event: any) => {
+    setDistance(event.target.value);
+  };
+  const [flipped, onflipped] = useState(false);
+  const onFlip = () => {
+    onflipped((current) => !current);
+    setDistance(0);
+  };
+  const onReset = () => {
+    setDistance(0);
+  };
+
+  return (
+    <>
+      <div>
+        <h3>KM</h3>
+        <input
+          value={flipped ? distance / 1000 : distance}
+          placeholder={"KM"}
+          type="number"
+          onChange={onDistance}
+          disabled={flipped}
+        />
+      </div>
+      <div>
+        <h3>M</h3>
+        <input
+          value={flipped ? distance : distance * 1000}
+          placeholder={"M"}
+          type="number"
+          onChange={onDistance}
+          disabled={!flipped}
+        />
+      </div>
+      <button onClick={onFlip}>{flipped ? "Turn back" : "Revert"}</button>
+      <button onClick={onReset}>Reset</button>
+    </>
+  );
+}
 
 function App() {
+  const [index, setIndex] = useState("0");
+  const onIndex = (event: any) => {
+    setIndex(event.target.value);
+  };
   return (
     <div className="App">
-      <Converter />
+      <h1>Super Converter</h1>
+      <select value={index} onChange={onIndex}>
+        <option value="0">Minutes & Hours</option>
+        <option value="1">Km & M</option>
+      </select>
+      {index === "0" ? <MinutesToHours /> : null}
+      {index === "1" ? <KmToM /> : null}
     </div>
   );
 }
